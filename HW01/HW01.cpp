2a4942105f8f52b374ae7b726192fa5f29bd20ed
nayoungjin39 <211291869+nayoungjin39@users.noreply.github.com>
2025-06-11 09:31:18 +0900

    Create HW01.cpp

---
+++ HW01/HW01.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int arr[5] = { 1, 2, 3, 4, 5 };
+	int a = 0;
+		for (int i = 0; i < 5; i++) {
+			a += arr[i];
+		}
+	double b = a / 5.0;
+
+	cout << "합계:" << a << endl;
+	cout << "통합:" << b << endl;
+
+	return 0;
+}
